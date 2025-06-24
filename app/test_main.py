833c5f5ad166bdb35b83e3c20a9bda9afc08e497
Masud Rana <masud.cseian@gmail.com>
2025-06-25 03:14:42 +0600

    add fail test

--- app/test_main.py
+++ app/test_main.py
@@ -7,3 +7,6 @@ def test_welcome():
     response = client.post("/welcome", json={"name": "A Name"})
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome A Name"}
+
+def test_fail():
+    assert 1 == 0
